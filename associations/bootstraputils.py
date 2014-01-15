efc92d1ed3d5871d961a3a8e66baaac481d89346
DarkSector <pronoyc@gmail.com>
2014-01-15 17:09:01 -0600

    Some documentation added to the method, needs reconsideration using regex

--- associations/bootstraputils.py
+++ associations/bootstraputils.py
@@ -62,11 +62,24 @@ def get_non_django_installed_apps():
     Returns a list of all the non django apps installed
     """
     list_of_installed_apps = []
-    list_of_python_installed_packages = pip.get_installed_distributions()
+    list_of_python_installed_packages = []
+
+    for distribution in pip.get_installed_distributions():
+        # somehow it gives an error if it's not string later on
+        list_of_python_installed_packages.append(str(distribution))
+
     django_settings = get_django_settings()
     installed_apps = django_settings.INSTALLED_APPS
+
+    #print installed_apps
     for app in installed_apps:
         if not 'django' in app:
-            if not app in list_of_python_installed_packages:
-                list_of_installed_apps.append(app)
+            # check if the name django doesn't come in
+            for dist_name in list_of_python_installed_packages:
+                # check if the name isn't in any individual app name
+                # in the installed python packages
+                if not app in dist_name:
+                    # if not present
+                    # allow the url to be parsed
+                    list_of_installed_apps.append(app)
     return list_of_installed_apps
