a982885f2b0aabcb035d9d4fa681418600aea7a6
DarkSector <pronoyc@gmail.com>
2014-01-15 15:38:32 -0600

    Updated existing filter decorator and created a new filter

--- associations/templatetags/detailed_tags.py
+++ associations/templatetags/detailed_tags.py
@@ -4,6 +4,11 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
+@register.filter
 def cut(value, arg):
     return value.replace(arg, '')
+
+@register.filter
+def replacewithappname(value, app_name):
+    replaced_app_name = '/'+app_name+'/'
+    return value.replace('^', replaced_app_name)
