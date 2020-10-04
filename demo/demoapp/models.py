ddc7a8bb00ec01770f828c99d3e282eeaebc0d87
Pronoy Chopra <pronoyc@gmail.com>
2020-10-03 19:57:26 -0700

    Removed clutter

--- demo/demoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Demo(models.Model):
-
-    name = models.CharField(max_length=150, unique=True)
-    pub_date = models.DateTimeField(auto_now=True)
-    pub_author = models.ForeignKey(User)
