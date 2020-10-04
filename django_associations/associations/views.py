c1c375036c05f0816339ce0ebfc034f3f42fa521
Pronoy Chopra <pronoyc@gmail.com>
2020-10-04 02:36:37 -0700

    Added a simple view to trigger api from the browser

--- django_associations/associations/views.py
+++ django_associations/associations/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from django.views.generic import TemplateView
+from .api import get_association_list
+
+
+class AssociationsView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(content=get_association_list())
