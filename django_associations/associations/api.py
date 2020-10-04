03c772be9d3a43331fed5d548f1c6581e0cb6dab
Pronoy Chopra <pronoyc@gmail.com>
2020-10-04 02:36:18 -0700

    Core api added

---
+++ django_associations/associations/api.py
@@ -0,0 +1,57 @@
+from django.conf import settings
+from django.urls import URLResolver
+from django_extensions.management.commands.show_urls import (URLPattern, describe_pattern,
+                                                             ViewDoesNotExist)
+
+
+def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
+    """
+    Return a list of views from a list of urlpatterns.
+
+    Each object in the returned list is a three-tuple: (view_func, regex, name)
+    """
+    views = []
+    for p in urlpatterns:
+        if isinstance(p, (URLPattern,)):
+            try:
+                if not p.name:
+                    name = p.name
+                elif namespace:
+                    name = '{0}:{1}'.format(namespace, p.name)
+                else:
+                    name = p.name
+                pattern = describe_pattern(p)
+                views.append((p.callback, base + pattern, name))
+            except ViewDoesNotExist:
+                continue
+        elif isinstance(p, (URLResolver,)):
+            try:
+                patterns = p.url_patterns
+            except ImportError:
+                continue
+            if namespace and p.namespace:
+                _namespace = '{0}:{1}'.format(namespace, p.namespace)
+            else:
+                _namespace = (p.namespace or namespace)
+            pattern = describe_pattern(p)
+            print(_namespace)
+            views.extend(extract_views_from_urlpatterns(patterns, base + pattern, namespace=_namespace))
+        elif hasattr(p, '_get_callback'):
+            try:
+                views.append((p._get_callback(), base + describe_pattern(p), p.name))
+            except ViewDoesNotExist:
+                continue
+        elif hasattr(p, 'url_patterns') or hasattr(p, '_get_url_patterns'):
+            try:
+                patterns = p.url_patterns
+            except ImportError:
+                continue
+            views.extend(extract_views_from_urlpatterns(patterns, base + describe_pattern(p), namespace=namespace))
+        else:
+            raise TypeError("%s does not appear to be a urlpattern object" % p)
+    return views
+
+
+def get_association_list():
+    urlconf = __import__(getattr(settings, "ROOT_URLCONF"), {}, {}, [''])
+    return extract_views_from_urlpatterns(urlconf.urlpatterns)
