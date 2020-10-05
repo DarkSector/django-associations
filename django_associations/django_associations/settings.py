6408275c5169ace490d4db028af3e282a648f914
Pronoy Chopra <pronoyc@gmail.com>
2020-10-05 12:10:12 +0500

    Extensions added to the duration of the development

--- django_associations/django_associations/settings.py
+++ django_associations/django_associations/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'associations',
     'testapp1',
     'testapp2'
